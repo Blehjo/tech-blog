06381b327b0b84fb18a8eb7b183d5b8e241f24fc
bseton11-YJKnqQ <bseton11@gmail.com>
2022-03-02 15:28:51 -0600

    created route for posting comments

--- public/js/post-comment.js
+++ public/js/post-comment.js
@@ -2,18 +2,18 @@ const commentFormHandler = async (event) => {
     event.preventDefault();
 
     const comment = document.querySelector('.comment-input').value.trim();
-    // const post_id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1];
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1];
 
-    if (comment) {
+    if (comment && post_id) {
         const response = await fetch('/api/comments', {
         method: 'POST',
-        body: JSON.stringify({ comment }),
+        body: JSON.stringify({ comment, post_id }),
         headers: { 'Content-Type': 'application/json' },
         });
 
         if (comment) {
-            document.location.replace('/');
+            document.location.replace(`/post/${post_id}`);
         } else {
             alert('Could not post comment.');
         }
